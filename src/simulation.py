67c84ae131731fc04d8f7edfa3076fbd1f173887
Sharky Bamboozle <login@jmayer.ai>
2022-04-14 17:20:13 +0200

    Integrate new image preprocessor and yaml config file to simulation module

--- src/simulation.py
+++ src/simulation.py
@@ -1,10 +1,9 @@
+#!/usr/bin/env python 3.9.7
 # -*- coding: utf-8 -*-
 
-"""
-Simulation class for the research project 'Cortical Spike Synchrony as 
+"""Simulation module for the research project 'Cortical Spike Synchrony as 
 a Measure of Contour Uniformity', as part of the RTG computational cognition, 
-Osnabrueck University, Germany.
-"""
+Osnabrueck University, Germany."""
 
 __author__    = 'Julius Mayer, Viktoria Zemliak, Flora Perizonius'
 __email__     = 'viktoriia.zemliak@uni-osnabrueck.de'
@@ -12,32 +11,40 @@ __date__      = '01.04.2022'
 __copyright__ = '(C) 2022 Julius Mayer, Viktoria Zemliak, Flora Perizonius'
 __license__   = 'MIT License'
 
+#==============================================================================#
+#========================|          Imports           |========================#
+#==============================================================================#
+
+#standard library imports
+import os 
+
+#related third party imports
 import matplotlib.pyplot as plt 
 import numpy as np
-import scipy as sp
-import time
+import yaml
 
-from skimage.draw import line_nd
-
+#local imports
 from network import Connectivity
 from model import Izhikevich
+from image_preprocessor import ImagePreprocessor
+from draw import line_45_joint
+
+#==============================================================================#
+#=======================|          Simulation          |=======================#
+#==============================================================================#
 
 class Simulation:
-    def __init__(self, stimulus,
-                 filters, filter_step=1, filter_start_width=0, filter_start_height=0, 
+    def __init__(self, stimulus, filters, 
                  angle_connect_strength=0.5, spatial_connect_strength=0.5, total_connect_strength=0.5,
                  inh=0.2, inh_weight=2.0, 
                  random_noise=0.0, input_firing_prob=1.0, random_seed=None,
                  input_strength=1.0, ini_standard=False
                  ):
-        """
-        Builds the connections network and runs the simulation of a given length.
+        """Builds the connections network and runs the simulation of a given length.
+        
         Args:
             stimulus = the input stimulus image
             filters = convolution filters for angles recognition
-            filter_step = step of the convolution filters
-            filter_start_width = the starting x point of the convolution filters
-            filter_start_height = the starting y point of the convolution filters
             angle_connect_strength = the strength of angular connections between neurons
             spatial_connect_strength = the strength of spatial connections between neurons
             total_connect_strength = the strength of final connections between neurons
@@ -47,15 +54,12 @@ class Simulation:
             input_firing_prob = probability of firing of the input neurons
             random_seed = random seed
         """
-        
-        if len(stimulus.shape) != 2:
-            raise Exception("Please use a 2-dimensional stimulus image")
-        
+
         #initialize the Connectivity class with filters
         self.connectivity = Connectivity(filters)
         
         #input preprocessing
-        self.neural_input = self._preprocess_input(stimulus, filter_step, filter_start_width, filter_start_height)
+        self.neural_input = stimulus
         
         #initialize helper simulation parameters
         network_shape = self.neural_input.shape
@@ -84,39 +88,51 @@ class Simulation:
                                      ini_standard=ini_standard, input_strength=input_strength,
                                      random_noise=random_noise, input_firing_prob=input_firing_prob, random_seed=random_seed
                                      )
-        
-    def _preprocess_input(self, stimulus, filter_step, filter_start_width, filter_start_height):
-        '''
-        Detect angles on the stimulus picture and return flattened output.
-        '''
-        filtered = self.connectivity.detect_angles(stimulus, filter_step, filter_start_width, filter_start_height)
-        filtered[filtered < 2] = 0
-        return filtered / 2
     
-    def run(self, length=1000, verbose=False):
+    #======================================================================#
+    #======================|     Run Simulation     |======================#
+    #=======================================================================#
+
+    def run(self, length=100, verbose=False):
         voltage, recovery, firings = self.izhikevich.simulate(self.neural_input, length, verbose)
         return voltage, recovery, firings
         
+#==============================================================================#
+#==========================|          Main          |==========================#
+#==============================================================================#
+    
 if __name__ == '__main__':
+
+    #========================|     Parameter      |========================#
+    #import experiment configurations
+    with open('config.yaml') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    #preprocessor parameter
+    angular_resolution =  config["preprocessing"]["angle_resolution"]
+    filter_size = config["preprocessing"]["filter_size"]
+    image_path = config["preprocessing"]["input_image"]
+    image_array = line_45_joint(width=28, height=28, strength=255, length=3)
+
     #simulation parameters
-    filters = [[[0,0],[1,1]], [[1,0],[0,1]], [[0,1],[0,1]], [[0,1],[1,0]]]
     inh = 0.2
     length = 1000
     ini_standard = True
     
-    #create stimulus image
-    height, width, strength = 10, 10, 1
-    stimulus = np.zeros(shape=(height,width))
-    rr, cc = line_nd((0, 0), (height, width))  
-    stimulus[rr, cc] = strength
-    
+    #======================|     Initialization     |======================#
+    #preprocessor instantiation and preprocessing of image
+    preprocessor = ImagePreprocessor(angular_resolution, filter_size)
+    stimulus = preprocessor.preprocess(image_array, plot_substeps=False)
+
     #initialize simulation object
     print('Initialize the simulation')
-    sim = Simulation(stimulus=stimulus, filters=filters, inh=inh, ini_standard=ini_standard)
-    #run simulation
+    sim = Simulation(stimulus=stimulus, filters=preprocessor.angle_filters, inh=inh, ini_standard=ini_standard)
+
+    #======================|     Run Simulation     |======================#
     print('Simulation started...')
     voltage, recovery, firings = sim.run(length, True)
     
+    #=========================|     Plotting     |=========================#
     #plot voltage
     h, w = voltage.shape
     fig, ax = plt.subplots(figsize=(8,8))
